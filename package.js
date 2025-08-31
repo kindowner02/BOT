45d2ac25824b577afdfb642e97322f1e96c98a11
kindowner02 <kindowner02@gmail.com>
2025-08-31 08:24:57 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "always_online_bot",
+  "version": "1.0.0",
+  "description": "Minecraft AFK Bot always online using Render",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mineflayer": "^4.15.0"
+  }
+}
